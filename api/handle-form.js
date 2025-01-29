25813b4dd043c793090986160d474db1e595c8dc
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 18:41:08 -0500

    feat: update file on handle form

--- api/handle-form.js
+++ api/handle-form.js
@@ -1,3 +1,11 @@
+import { updateGithubFile } from "../lib/update-github-file.js";
+
+// Set the file path and file string
+// Later, we expect these to be set dynamically through forms
+const filePath = "content/content-test.json";
+const fileString = `{ "some-key": "another update from Vercel function" }`;
+
+
 // Define the handler
 export async function POST(request) {
 	const formData = await request.formData();
@@ -6,13 +14,17 @@ export async function POST(request) {
 		parsedFormData[key] = value
 	})
 
+	const fileString = JSON.stringify(parsedFormData, null, 2);
+
+	// Update the file
+	const updateResult = await updateGithubFile(filePath, fileString);
+
 	const debugData = {
-		// rawBody: await request.text(),
-		// parsedJson: await request.json(),
 		parsedFormData,
 		parsedBody: request.body,
 		method: request.method,
-		url: request.url
+		url: request.url,
+		updateResult
 	}
 	return new Response(
 		`Submitted form data: ${JSON.stringify(
@@ -22,3 +34,4 @@ export async function POST(request) {
 		{ status: 200 }
 	);
 }
+
