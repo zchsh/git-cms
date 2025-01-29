34c7b5294ef09ea711ad42c0e133dc798bfcf246
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 17:22:35 -0500

    fix: return result from updateGithubFile

--- api/hello.js
+++ api/hello.js
@@ -3,7 +3,7 @@ import { updateGithubFile } from "../lib/update-github-file.js";
 // Set the file path and file string
 // Later, we expect these to be set dynamically through forms
 const filePath = "content/content-test.json";
-const fileString = `{ "some-key": "some value updated from Vercel function" }`;
+const fileString = `{ "some-key": "another update from Vercel function" }`;
 
 // Define the handler
 export async function GET(request) {
