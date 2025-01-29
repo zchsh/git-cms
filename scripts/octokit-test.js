1b6f1b4e98ea8ea317bd47f854c4ba4931b0319a
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 16:55:41 -0500

    feat: add octokit script

--- scripts/octokit-test.js
+++ scripts/octokit-test.js
@@ -1,16 +1,15 @@
-// Octokit.js
+import { Octokit } from "octokit";
+
+// Initialize Octokit.js
 // https://github.com/octokit/core.js#readme
 const octokit = new Octokit({
-	auth: "YOUR-TOKEN",
+	auth: process.env.GITHUB_TOKEN,
 });
 
 // Set the file path and file string
 // Later, we expect these to be set dynamically through forms
 const filePath = "content/content-test.json";
 const fileString = `{ "content": "update" }`;
-
-// Encode the file string in base64
-const fileStringEncoded = Buffer.from(fileString).toString("base64");
 
 // Get the SHA of the file at the specified path
 const existingFile = await octokit.request(
@@ -19,23 +18,29 @@ const existingFile = await octokit.request(
 		owner: "zchsh",
 		repo: "git-cms",
 		path: filePath,
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28",
+		},
 	}
 );
 
 console.log(existingFile);
 
-// await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-// 	owner: "OWNER",
-// 	repo: "REPO",
-// 	path: "PATH",
-// 	message: "a new commit message",
-// 	committer: {
-// 		name: "Zach Shilton",
-// 		email: "4624598+zchsh@users.noreply.github.com",
-// 	},
-// 	content: fileStringEncoded,
-// 	sha: "95b966ae1c166bd92f8ae7d1c313e738c731dfc3",
-// 	headers: {
-// 		"X-GitHub-Api-Version": "2022-11-28",
-// 	},
-// });
+// Encode the file string in base64
+const fileStringEncoded = Buffer.from(fileString).toString("base64");
+
+await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+	owner: "zchsh",
+	repo: "git-cms",
+	path: filePath,
+	message: "chore: test programmatic content update",
+	committer: {
+		name: "Zach Shilton",
+		email: "4624598+zchsh@users.noreply.github.com",
+	},
+	content: fileStringEncoded,
+	sha: "95b966ae1c166bd92f8ae7d1c313e738c731dfc3",
+	headers: {
+		"X-GitHub-Api-Version": "2022-11-28",
+	},
+});
