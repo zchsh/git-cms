30a3769cc9b1b946c4c708f31fd175e39f1830b3
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 17:14:00 -0500

    chore: test content update

--- scripts/octokit-test.js
+++ scripts/octokit-test.js
@@ -3,7 +3,7 @@ import { updateGithubFile } from "../lib/update-github-file.js";
 // Set the file path and file string
 // Later, we expect these to be set dynamically through forms
 const filePath = "content/content-test.json";
-const fileString = `{ "content": "updated from script in module" }`;
+const fileString = `{ "some-key": "some value updated from script" }`;
 
 // Update the file
 await updateGithubFile(filePath, fileString);
