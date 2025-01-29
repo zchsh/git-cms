d436f9253a31d52d16255f74bc9c5b97d51d220b
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 18:38:05 -0500

    chore: try parsing form data

--- api/handle-form.js
+++ api/handle-form.js
@@ -1,9 +1,15 @@
 // Define the handler
 export async function POST(request) {
+	const formData = await request.formData();
+	const parsedFormData = {};
+	formData.forEach((value, key) => {
+		parsedFormData[key] = value
+	})
+
 	const debugData = {
 		// rawBody: await request.text(),
 		// parsedJson: await request.json(),
-		parsedFormData: await request.formData(),
+		parsedFormData,
 		parsedBody: request.body,
 		method: request.method,
 		url: request.url
