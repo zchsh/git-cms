8ba0b1c921f517b985221635e8d6e30a6782f2f9
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 17:17:33 -0500

    feat: stub in endpoint

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,21 @@
-export function GET(request) {
-	return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
+import { updateGithubFile } from "../lib/update-github-file.js";
 
+// Configure the function for Vercel
 export const config = {
 	runtime: "nodejs",
 };
+
+// Set the file path and file string
+// Later, we expect these to be set dynamically through forms
+const filePath = "content/content-test.json";
+const fileString = `{ "some-key": "some value updated from Vercel function" }`;
+
+// Define the handler
+export async function GET(request) {
+	// Update the file
+	const result = await updateGithubFile(filePath, fileString);
+	// Return the update file contents
+	return new Response(
+		`Hello from ${process.env.VERCEL_REGION}. Result: ${result}`
+	);
+}
