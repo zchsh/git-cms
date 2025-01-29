edcf46afe1e43800ed996c662ca87fabef2199b9
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 18:09:24 -0500

    chore: debug form data

--- api/handle-form.js
+++ api/handle-form.js
@@ -1,8 +1,18 @@
 // Define the handler
-export async function POST(request) {
-	const requestBody = request.body;
+export async function GET(request) {
+	const rawBody = await request.text();
+	const parsedBody = request.body;
+  const parsedFormData = request.formData
 	return new Response(
-		`Submitted form data: ${JSON.stringify({ requestBody }, null, 2)}`,
+		`Submitted form data: ${rawBody}. Parsed body: ${JSON.stringify(
+			parsedBody,
+			null,
+			2
+		)}. Parsed body: ${JSON.stringify(
+			parsedFormData,
+			null,
+			2
+		)}`,
 		{ status: 200 }
 	);
 }
