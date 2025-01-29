525713ea8f9f267c206de9821b59e3a319fe1bdd
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-29 17:53:17 -0500

    chore: try to simplify form

--- api/handle-form.js
+++ api/handle-form.js
@@ -1,22 +1,8 @@
-import multiparty from "multiparty";
-
 // Define the handler
 export async function POST(request) {
-	const form = new multiparty.Form();
-	const formData = await new Promise((resolve, reject) => {
-		form.parse(request, function (err, fields, files) {
-			if (err) reject({ err });
-			resolve({ fields, files });
-		});
-	});
-
-	console.log(`Submitted form data: ${JSON.stringify(formData, null, 2)}`);
-
-	return new Response("{}", { status: 200 });
+	const requestBody = request.body;
+	return new Response(
+		`Submitted form data: ${JSON.stringify({ requestBody }, null, 2)}`,
+		{ status: 200 }
+	);
 }
-
-export const config = {
-	api: {
-		bodyParser: false,
-	},
-};
