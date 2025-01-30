4f9bf67e96493a78a31af42f35224d7936e99523
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-01-30 15:19:02 -0500

    feat: add build script

--- scripts/build.js
+++ scripts/build.js
@@ -2,16 +2,17 @@ import fs from "fs"
 import path from "path"
 
 const contentFile = path.join(process.cwd(), "content", "content-test.json")
-const targetFile = path.join(process.cwd(), "public", "index.html")
+const inputFilePath = path.join(process.cwd(), "src", "index.html")
+const outputFilePath = path.join(process.cwd(), "public", "index.html")
 
-// Read in the targetFile
-const inputFileString = fs.readFileSync(targetFile, "utf-8")
+// Read in the input file
+const inputFileString = fs.readFileSync(inputFilePath, "utf-8")
 
 // Read in the content file
-const contentFileString = fs.readFileSync(targetFile, "utf-8")
+const contentFileString = fs.readFileSync(contentFile, "utf-8")
 
 // Replace `{{CONTENT}}` in the file with stringified content
-// TODO
+const outputFileString = inputFileString.replace("{{CONTENT}}", contentFileString)
 
 // Write out modified public/index.html
-// TODO
+fs.writeFileSync(outputFilePath, outputFileString)
